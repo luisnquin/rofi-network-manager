b76dba4eea771525561b9d0a48f914c2940c76aa
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-10-08 22:41:41 -0600

    feat(nix): updated packaging

--- default.nix
+++ default.nix
@@ -22,12 +22,18 @@ with pkgs;
     postPatch = ''
       substituteInPlace ./${name}.bash \
         --replace 'DIR="$(cd "$(dirname "''${BASH_SOURCE[0]}")" && pwd)"' 'DIR=${placeholder "out"}/share'
+
+      substituteInPlace ./${name}.bash \
+        --replace 'ICONS_DIR="$PWD/assets/icons"' 'ICONS_DIR=${placeholder "out"}/share/icons'
     '';
 
     installPhase = ''
       runHook preInstall
 
-      mkdir -p $out/bin/
+      mkdir -p $out/share/icons
+      cp ./assets/icons/* $out/share/icons
+
+      mkdir -p $out/bin
       cp ./${name}.bash $out/bin/${name}
       chmod +x $out/bin/${name}
 
